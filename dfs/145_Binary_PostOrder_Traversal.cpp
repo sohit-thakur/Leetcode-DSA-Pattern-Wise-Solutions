7b0e51dd7feccb6f9e0b20369cad7496818b4cc9
thakursohit <sohitthakur26@gmail.com>
2026-02-02 14:30:42 +0530

    stack is good for large tree

---
+++ dfs/145_Binary_PostOrder_Traversal.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==nullptr) return ans;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* node = st1.top();
+            st2.push(node);
+            st1.pop();
+            if(node->left) st1.push(node->left);
+            if(node->right) st1.push(node->right);
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
+    }
+};
