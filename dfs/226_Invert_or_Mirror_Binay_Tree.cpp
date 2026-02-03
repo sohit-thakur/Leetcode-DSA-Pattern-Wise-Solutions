93e8153d6e64b0e46559e7f4cc64c8bab4bba081
thakursohit <sohitthakur26@gmail.com>
2026-02-03 11:51:37 +0530

    Invert or Mirror

---
+++ dfs/226_Invert_or_Mirror_Binay_Tree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr) return 0;
+        swap(root->left,root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
